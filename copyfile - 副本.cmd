f3dfe9e14f84d5ee62635c0427af4f2398c3f874
Charlie Jiang <cqjjjzr@126.com>
2016-12-13 13:53:50 +0800

    Delete copyfile - 副本.cmd

--- copyfile - 副本.cmd
+++
@@ -1,3 +0,0 @@
-copy "C:\Users\cqjjj\Documents\Visual Studio 2015\Projects\iTunesSupport\iTunesSupportImpl\bin\Debug\*.dll" "C:\Program Files\Studio GPBeta\SAO Utils\"
-copy "C:\Users\cqjjj\Documents\Visual Studio 2015\Projects\iTunesSupport\x64\Debug\module64.dll" "C:\Program Files\Studio GPBeta\SAO Utils\Plugins\iTunes Support\"
-"C:\Program Files\Studio GPBeta\SAO Utils\SAO Utils.exe"
