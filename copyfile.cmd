6aeaf1408433c59e684a17d559ab12e79badba6b
Charlie Jiang <cqjjjzr@126.com>
2016-12-13 13:53:41 +0800

    Delete copyfile.cmd

--- copyfile.cmd
+++
@@ -1,3 +0,0 @@
-copy "C:\Users\cqjjj\Documents\Visual Studio 2015\Projects\iTunesSupport\iTunesSupportImpl\bin\Debug\*.dll" "D:\NERvSDK\bin\win64\"
-copy "C:\Users\cqjjj\Documents\Visual Studio 2015\Projects\iTunesSupport\x64\Debug\module64.dll" "D:\NERvSDK\bin\win64\Plugins\iTunes Support\"
-"D:\NERvSDK\bin\win64\NERvSDK.exe"
